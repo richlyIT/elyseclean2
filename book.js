342fada679bf214cbfcd29084107ade7846a2c08
Elohim242 <99142926+Elohim242@users.noreply.github.com>
2022-08-02 00:05:02 +0800

    php

---
+++ book.js
@@ -0,0 +1,65 @@
+//==========OPEN & CLOSE TICKET==========
+
+
+
+
+
+
+
+
+if (document.readyState == "loading") {
+    document.addEventListener('DOMContentLoaded', start);
+} else {
+    start();
+}
+
+
+
+
+
+
+
+//=================== Start ===================
+
+function start() {
+
+}
+
+
+
+
+//=============== Update & RERENDER ================
+
+function update() {
+
+}
+
+
+//=============== Add Events =============
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Deo's code
+
+
+//var input1 = document.querySelectorAll(".ticketVar");
+//input1.forEach((element) => { console.log(element.firstChild); })
+
+
+//Value of the input areas
+//var test = document.getElementById('service_type');
+//console.log(test.firstChild);
---
+++ book.php
@@ -0,0 +1,41 @@
+<?php
+$nom = "neo lucem";
+
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;          //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp-relay.sendinblue.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'richlystudiosit@gmail.com';                     //SMTP username
+    $mail->Password   = 'd0vRb6BzSOjFm5gD';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('richlystudiosit@gmail.com', 'Richly Studios');
+    $mail->addAddress('deoobomby@gmail.com');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>'.$nom;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
