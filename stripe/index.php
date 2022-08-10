2f5305bd0f08106e7ec9952ebc69d58d5149dade
NeoLucem <98679519+NeoLucem@users.noreply.github.com>
2022-08-10 13:24:27 +0000

    change the place of stripe js script link

--- stripe/index.php
+++ stripe/index.php
@@ -106,9 +106,12 @@
         </div>
         </div>
     </div>
-
-        <!------------jquery--------->
-        <script src="../assets/vender/jquery/jquery.js"></script>
+    
+    <!-- Javascript script -->
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="checkout.js" defer></script>
+    <!------------jquery--------->
+    <script src="../assets/vender/jquery/jquery.js"></script>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <!-- Bootstrap JavaScript Libraries -->
@@ -123,9 +126,6 @@
     <script src="../assets/vender/owl-carosel/owl.carousel.min.js"></script>
     <!-- Javascript script -->
     <script src="../assets/js/main.js"></script>
-    <!-- Javascript script -->
-    <script src="https://js.stripe.com/v3/"></script>
-    <script src="checkout.js" defer></script>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
